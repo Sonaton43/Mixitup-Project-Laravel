2dab2dc6d0a4456c6f29f2456c014640cd3681b3
Sonaton Kundu <91369056+Sonaton43@users.noreply.github.com>
2024-10-07 17:44:19 +0600

    first and last commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+    $('.header-area').ripples({
+  
+      // Image Url
+      imageUrl: null,
+      resolution: 256,
+      dropRadius: 20,
+      perturbance: 0.04,
+      interactive: true, 
+      crossOrigin: '',
+    });
+
+  });
